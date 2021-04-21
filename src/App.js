57002785941cb6181e455de8101b1cad4924e745
Winston Van <wvan@laurentian.ca>
2021-04-20 20:04:13 -0400

    checkbox state change handler

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
 import React from "react";
 import TodoItem from "./TodoItem";
 import todosData from "./todosData";
+import "./style.css";
 
-function App() {
-  const todoItems = todosData.map((item) => (
-    <TodoItem key={item.id} item={item} />
-  ));
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todosData, // import data from todosData.js to todos array
+    };
 
-  return <div className="todo-list">{todoItems}</div>;
+    // binds handleChange method to this class
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(id) {
+    this.setState(function (prevState) {
+      // iterate through state todo to find matching id of the checkbox that was changed
+      const newTodoList = prevState.todos.map(function (todo) {
+        if (todo.id === id) {
+          // returns a new todo object to avoid modifying current state
+          return {
+            ...todo, // pull all of the todo data otherwise it's just returning an object with a single completed state
+            completed: !todo.completed,
+          };
+        }
+        return todo; // return untouched todo item
+      });
+
+      return {
+        todos: newTodoList, // replace todo state with new modified one
+      };
+    });
+  }
+
+  render() {
+    const todoItems = this.state.todos.map((item) => (
+      <TodoItem key={item.id} item={item} handleChange={this.handleChange} />
+    ));
+
+    return <div className="todo-list">{todoItems}</div>;
+  }
 }
 
 export default App;
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,7 +3,13 @@ import React from "react";
 function TodoItem(props) {
   return (
     <div className="todo-item">
-      <input type="checkbox" checked={props.item.completed} />
+      <input
+        type="checkbox"
+        checked={props.item.completed}
+        onChange={function (event) {
+          props.handleChange(props.item.id);
+        }}
+      />
       <p>{props.item.text}</p>
     </div>
   );
